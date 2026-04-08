f24aabe9cebd2d9d2bd1c83f5ef5528d1da8887a
BinBin826 <nthanhtuan0802@gmail.com>
2026-04-08 14:45:58 +0700

    ok

--- NoiThat/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../includes/functions.php';
-unset($_SESSION['admin_id'], $_SESSION['admin_name']);
-redirect(SITE_URL . '/admin/login.php');
--- NoiThat/auth/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../includes/functions.php';
-session_destroy();
-redirect(SITE_URL . '/auth/login.php');
